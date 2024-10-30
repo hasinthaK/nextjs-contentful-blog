41d1e6281ca2e3d1b28ae8fb316fa8780d49a2a2
Hasintha Abeykoon <hasintha.a@eight25media.com>
2024-10-30 10:32:17 +0530

    feature/add-rich-text-field-rendering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import { createClient } from "contentful";
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 
 const client = createClient({
   space: process.env.SPACE_ID!,
   accessToken: process.env.ACCESS_TOKEN!,
-  environment: 'dev'
+  // environment: "dev",
 });
 
 const getBlogEntries = async () => {
@@ -18,6 +19,15 @@ export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <p>blog listing goes here</p>
+
+      <ul>
+        {blogEntries.items.map(({ fields, sys }) => (
+          <li key={sys.id}>
+            {fields.name}
+            {documentToReactComponents(fields.content)}
+            </li>
+        ))}
+      </ul>
     </main>
   );
 }
